3414ae0c4e3abf8b48182c1b52924f3bb5e61776
cypeng <Chien.Y.Peng@gmail.com>
2016-10-21 14:09:37 -0700

    Added 'css/additional_style.css' to take out the line block indentation in HTML tables.

--- src/conf.py
+++ src/conf.py
@@ -103,6 +103,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
   import sphinx_rtd_theme
   html_theme = 'sphinx_rtd_theme'
   html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+  html_style = 'css/additional_style.css'
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
