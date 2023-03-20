3909107490989ae39e4397dbdb8347795ec6788e
William Schoenell <wschoenell@gmto.org>
2023-03-20 10:25:24 -0700

    removed isample codegen

--- src/software_development/development/codegen/isample_dcs/cpp/ControlMode.h
+++
@@ -1,4 +0,0 @@
-
-enum class ControlMode : std::uint8_t { STANDALONE=0, INTEGRATED=1 };
-MSGPACK_ADD_ENUM(ControlMode)
-
--- src/software_development/development/codegen/isample_dcs/cpp/SimulationMode.h
+++
@@ -1,4 +0,0 @@
-
-enum class SimulationMode : std::uint8_t { ON_LINE=0, SIMULATED=1 };
-MSGPACK_ADD_ENUM(SimulationMode)
-
